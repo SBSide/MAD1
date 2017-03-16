5903f7cd53fe7bb04d27eb983fe0cad4d9d5f589
SBSide <ivecis@live.co.kr>
2017-03-16 14:20:52 +0900

    second.

--- app/src/main/java/com/example/iveci/mad1/MainActivity.java
+++ app/src/main/java/com/example/iveci/mad1/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.iveci.mad1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    RadioButton r1, r2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sample);
+
+    }
+
+    public void onMyClick(View v){
+
+    }
+
+    void init(){
+        r1 = (RadioButton)findViewById(R.id.radioButton);
+        r2 = (RadioButton)findViewById(R.id.radioButton2);
+        r1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getApplicationContext(),"고등학생이 선택됨.",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+        r2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getApplicationContext(),"대학생이 선택됨.",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
